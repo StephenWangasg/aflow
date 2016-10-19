d4a2951f9ea83ebfc906ee08f1bf8059a39ead68
Zhang Xuejie <zhangxuejie@gmail.com>
2016-10-19 01:34:56 +0000

    error catching for other scripts

--- flow/feature.py
+++ flow/feature.py
@@ -2,6 +2,7 @@ import time, os
 from flow.config import segmentation_server, classification_server, collection, feed_images_path
 from requests.exceptions import ConnectionError
 from flow.utils import ProductFeature, download_image_from_url
+import urllib2
 
 def redis_to_mongo():
     Features = ProductFeature(segmentation_server, classification_server)
@@ -14,6 +15,10 @@ def redis_to_mongo():
                     download_image_from_url(product['image_url'], img_path)
                 except (KeyboardInterrupt, SystemExit):
                     raise
+                except ValueError:
+                    collection.update_one({'image_path': img_path}, {'$set': {'extracted':'download_error_url'}})
+                except urllib2.HTTPError:
+                    collection.update_one({'image_path': img_path}, {'$set': {'extracted':'download_error_url_404'}})
                 except Exception as e:
                     print e
                     raise
@@ -26,20 +31,19 @@ def redis_to_mongo():
             print e
             time.sleep(600)
         except SyntaxError, e:
-            print e
-            collection.update_one({'image_path': img_path}, {'$set': {'extracted':None}})
-            raise
+            collection.update_one({'image_path': img_path}, {'$set': {'extracted':'server_error'}})
+            
         except Exception as e:
             print e
             raise
+            print img_path
 def change_paths():
     for row in collection.find({},{'image_path':1, 'image_name':1}):
         if 'new_models' in row['image_path']:
             img_path = feed_images_path + row['image_name']
-            print img_path
 #            break
             collection.update_one({'image_name': row['image_name']}, {'$set': {'image_path':img_path}})
 
 if __name__ == '__main__':
-#    redis_to_mongo()
+    redis_to_mongo()
     change_paths()
