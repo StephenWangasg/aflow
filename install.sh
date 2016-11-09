3fd25b82f2ed4250fae65d9e6661277beb11c5fe
zhangxuejie <zhangxuejie@gmail.com>
2016-11-09 14:27:42 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 # install mongo https://docs.mongodb.org/manual/tutorial/install-mongodb-on-ubuntu/
 export LC_ALL=C
 sudo pip install pymongo
+
+# install PIL
+sudo apt-get build-dep python-imaging
+sudo apt-get install libjpeg8 libjpeg62-dev libfreetype6 libfreetype6-dev
+sudo pip install Pillow
+
+# install boto
+pip install boto
