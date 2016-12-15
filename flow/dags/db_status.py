ae6236bdcd76cead578b0c2585e13980843520b2
Ubuntu <ubuntu@ip-172-31-29-35.ap-southeast-1.compute.internal>
2016-12-15 06:36:32 +0000

    Added individual args for db_status and gmv task

--- flow/dags/db_status.py
+++ flow/dags/db_status.py
@@ -4,10 +4,10 @@ sys.path.insert(0, flow_folder)
 
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
-from flow.dags.utils import asos_args
+from flow.dags.utils import db_status_args
 from flow.get_db_status import get_db_status
 
-dag = DAG('db_status', default_args=asos_args)
+dag = DAG('db_status', default_args=db_status_args)
 
 t1 = PythonOperator(
     task_id='db_status',
